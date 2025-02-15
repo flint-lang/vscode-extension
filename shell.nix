eb116828456037087c09ae66a75f3516030e2e8a
Marc Zweiler <marc.zweiler@outlook.at>
2025-02-15 02:04:51 +0100

    Added missing check if the docker container doesnt exist yet, added automatic building of said docker container if its missing

--- shell.nix
+++ shell.nix
@@ -5,6 +5,11 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
+    # Check if the 'yo-code-generator' image exists. If not, create it
+    if ! docker image inspect yo-code-generator > /dev/null 2>&1; then
+      echo "Building Docker image 'yo-code-generator'..."
+      docker build -t yo-code-generator .
+    fi
     docker run -it -v $(pwd):/app yo-code-generator
   '';
 }
